c729dec22f26a33d516a2d4a9718f2ff8b035593
Zeno <BMorter10@gmail.com>
2023-12-24 11:51:18 -0800

    Added files for Project5_comparisons_type_coercion

---
+++ Basic Javascript Projects/Project5_comparisons_type_coercions/JS/main.js
@@ -0,0 +1,23 @@
+function myTypeFunction() {
+    document.write(typeof 1996)
+}
+
+function myCoercionFunction() {
+    document.write("199" + 6)
+}
+
+function greaterFunction() {
+    document.write(10>5)
+}
+
+function lessFunction() {
+    document.write(10<5)
+}
+
+function equalFunction() {
+    document.write(5==5)
+}
+
+function andFunction() {
+    document.write(4>3 && 9>10)
+}
